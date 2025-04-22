a7845a166895c28fa40e722c166d5546fefc5b32
Martin Rioja <cr.martin.24680@gmail.com>
2025-04-22 10:56:43 -0500

    Update set-env.ts

--- set-env.ts
+++ set-env.ts
@@ -1,17 +1,28 @@
-const { writeFileSync } = require('fs');
-const dotenv = require('dotenv');
-const path = require('path');
+// set-env.ts
+import * as dotenv from 'dotenv';
+import * as fs from 'fs';
+import * as path from 'path';
 
-dotenv.config({ path: path.resolve(__dirname, '.env') });
+dotenv.config(); // Lee .env o las variables de entorno inyectadas por Vercel
 
-const targetPath = './src/environments/environment.ts';
+const targetPath = path.resolve(__dirname, './src/environments/environment.ts');
+const prodPath = path.resolve(__dirname, './src/environments/environment.prod.ts');
 
-const envConfigFile = `export const environment = {
+const envConfig = `
+export const environment = {
+  production: false,
+  base: '${process.env['NG_APP_API_BASE_URL']}',
+  dom: '${process.env['NG_APP_API_BASE_URL_DOM']}'
+};`;
+
+const prodEnvConfig = `
+export const environment = {
   production: true,
   base: '${process.env['NG_APP_API_BASE_URL']}',
   dom: '${process.env['NG_APP_API_BASE_URL_DOM']}'
 };`;
 
-writeFileSync(targetPath, envConfigFile, { encoding: 'utf-8' });
+fs.writeFileSync(targetPath, envConfig, { encoding: 'utf-8' });
+fs.writeFileSync(prodPath, prodEnvConfig, { encoding: 'utf-8' });
 
-console.log('✔️ Archivo environment.ts actualizado correctamente');
+console.log('✔ Entorno generado exitosamente ✅');
