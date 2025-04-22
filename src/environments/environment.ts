cf062aea7a1aa2e63cb277a000c9f27ea16d9cca
Martin Rioja <cr.martin.24680@gmail.com>
2025-04-22 08:27:22 -0500

    Env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
-export const environment = {
+export const environment ={
     production: true,
     base: process.env['NG_APP_API_BASE_URL'],
     dom: process.env['NG_APP_API_BASE_URL_DOM'], 
-}
+};
